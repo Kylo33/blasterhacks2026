816cae34fd0b52afa4c948d13b0136022f81c62a
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2026-04-04 12:03:42 -0600

    add context manager class

---
+++ ast-parser/context.ts
@@ -0,0 +1,26 @@
+import "./types.js"
+
+class Context {
+    #table!: Map<string, Varia>
+    #existingContext!: Context
+
+    constructor(existingContext: Context) {
+        this.#existingContext = existingContext;
+    }
+
+    put(id: string, v: Varia) {
+        this.#table.set(id, v)
+    }
+
+    get(id: string): Varia | undefined {
+        let cur: Context = this;
+        while (cur != undefined) {
+            let val = cur.#table.get(id);
+            if (val != undefined) {
+                return val
+            }
+            cur = this.#existingContext;
+        }
+        return undefined;
+    }
+}
